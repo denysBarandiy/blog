3fbe01a107fa53215d62f72a9f1d051992a67365
denysBarandiy <148606819+denysBarandiy@users.noreply.github.com>
2024-03-30 10:30:06 +0200

    init commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+const mongoose = require('mongoose')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,3 +13,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+mongodb.conect(`mongodb+srv://bert4457:leXXGU6nsBvwzK4C@blog.1xfieow.mongodb.net/?retryWrites=true&w=majority&appName=blog`)
+ .then(() => console.log('Connected to MongoDB'))
+.cath(err => console.error('Failed to connect to MongoDB'))
+
